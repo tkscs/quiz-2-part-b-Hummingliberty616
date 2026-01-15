6176a81d0bd89ef75fb2c77ec9e8aa42fc72ccc5
Hummingliberty616 <wlin2028@kehillahstudent.org>
2026-01-15 09:24:31 -0800

    -

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+current_state = "Forest"
+while True:
+    if current_state == "Forest":
+        current_input = ("You can go north (N) or go east (E). " \
+        "Which way would you like to go?")
 
+    if current_state == "River":
+        current_input = ("You can return to the forest (R) or swim (S)."
+        "Which would you like to do?")
+    
+    if current_state == "Win":
+        print("Congratulations! You won.")
+        break
+
+    if current_state == "Lose":
+        print("Sorry, you have lost.")
+        break
+    
+    if current_state == "Forest":
+        if current_input == "N":
+            current_state = "Win" 
+        elif current_input == "E":
+            current_state = "River"
+    
+    if current_state == "River":
+        if current_input == "R":
+            current_state = "Forest"
+        elif current_input == "Swim":
+            current_state = "Lose"
+    
